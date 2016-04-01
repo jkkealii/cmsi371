f8f7a7d4f34619a5e15d7578260959396a16f88d
Josh <jkkealii@gmail.com>
2016-03-31 23:47:10 -0700

    tested everything that's not gl-related

--- pipeline/tests/shape-test.js
+++ pipeline/tests/shape-test.js
@@ -37,6 +37,10 @@ QUnit.test("rotate test", function ( assert ) {
             Shapes.blanky(), 
             null, "LINE", 
             { x: 0.0, y: 1.0, z: 0.0 });
+    var thapeKid = new Shape({ r: 0.0, g: 0.00, b: 0.45 }, 
+            Shapes.blanky(), 
+            null, "LINE", 
+            { x: 0.0, y: 1.0, z: 0.0 });
     var rotatedThape1 = thape.rotate( Math.PI, Math.PI, Math.PI, Math.PI );
     var check = new Matrix([ 
             [0.9989980999092426, 0.03214186087161047, -0.031139960780853033, 0],
@@ -53,12 +57,19 @@ QUnit.test("rotate test", function ( assert ) {
             [0.0045216250067130195, -0.026951425539982213, 0.9996265180399435, 0.0],
             [0.0, 0.0, 0.0, 1.0]
         ]);
-    assert.deepEqual( rotatedThape2.rotation, check1, "rotate pass" );
+    assert.deepEqual( rotatedThape2.rotation, check1, "2nd pass" );
+
+    var rotatedKid = thape.manufactureYoungster(thapeKid).rotate( Math.PI, Math.PI, Math.PI, Math.PI );
+    assert.deepEqual( rotatedKid.rotation, check, "kid rotate pass" );
 });
 
 QUnit.test("translate test", function ( assert ) {
     var thape = new Shape({ r: 0.0, g: 0.00, b: 0.45 }, 
             Shapes.pointy(), 
+            null, "LINE", 
+            { x: 0.0, y: 1.0, z: 0.0 });
+    var thapeKid = new Shape({ r: 0.0, g: 0.00, b: 0.45 }, 
+            Shapes.blanky(), 
             null, "LINE", 
             { x: 0.0, y: 1.0, z: 0.0 });
     var translatedThape1 = thape.translate( 6, 9, 12 );
@@ -69,11 +80,18 @@ QUnit.test("translate test", function ( assert ) {
             [0, 0, 0, 1]
         ]);
     assert.deepEqual( translatedThape1.translation, check, "trans pass" );
+
+    var translatedKid = thape.manufactureYoungster(thapeKid).translate( 6, 9, 12 );
+    assert.deepEqual( translatedKid.translation, check, "kid trans pass" );
 });
 
 QUnit.test("scale test", function ( assert ) {
     var thape = new Shape({ r: 0.0, g: 0.00, b: 0.45 }, 
             Shapes.pointy(), 
+            null, "LINE", 
+            { x: 0.0, y: 1.0, z: 0.0 });
+    var thapeKid = new Shape({ r: 0.0, g: 0.00, b: 0.45 }, 
+            Shapes.blanky(), 
             null, "LINE", 
             { x: 0.0, y: 1.0, z: 0.0 });
     var scaledThape1 = thape.scale( 3, 8, 13 );
@@ -84,6 +102,9 @@ QUnit.test("scale test", function ( assert ) {
             [0, 0, 0, 1]
         ]);
     assert.deepEqual( scaledThape1.scaling, check, "scale pass" );
+
+    var scaledKid = thape.manufactureYoungster(thapeKid).scale( 3, 8, 13 );
+    assert.deepEqual( scaledKid.scaling, check, "kid scale pass" );
 });
 
 QUnit.test("stash/refresh test", function ( assert ) {
