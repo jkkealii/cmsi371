e6825fe6e3a7d270e6b624669bca0aa61ad3525a
Josh <jkkealii@gmail.com>
2016-03-31 23:53:12 -0700

    cleaning up

--- pipeline/3Dshapes.js
+++ pipeline/3Dshapes.js
@@ -19,88 +19,7 @@
         i,
         maxi,
         j,
-        maxj,
-
-
-        getRotationMatrix = function (angle, x, y, z) {
-            var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
-                s = Math.sin(angle * Math.PI / 180.0),
-                c = Math.cos(angle * Math.PI / 180.0),
-                oneMinusC = 1.0 - c,
-                x2,
-                y2,
-                z2,
-                xy,
-                yz,
-                xz,
-                xs,
-                ys,
-                zs;
-
-            x /= axisLength;
-            y /= axisLength;
-            z /= axisLength;
-
-            x2 = x * x;
-            y2 = y * y;
-            z2 = z * z;
-            xy = x * y;
-            yz = y * z;
-            xz = x * z;
-            xs = x * s;
-            ys = y * s;
-            zs = z * s;
-
-            return [
-                (x2 * oneMinusC) + c,
-                (xy * oneMinusC) + zs,
-                (xz * oneMinusC) - ys,
-                0.0,
-
-                (xy * oneMinusC) - zs,
-                (y2 * oneMinusC) + c,
-                (yz * oneMinusC) + xs,
-                0.0,
-
-                (xz * oneMinusC) + ys,
-                (yz * oneMinusC) - xs,
-                (z2 * oneMinusC) + c,
-                0.0,
-
-                0.0,
-                0.0,
-                0.0,
-                1.0
-            ];
-        },
-
-        getOrthoMatrix = function (left, right, bottom, top, zNear, zFar) {
-            var width = right - left,
-                height = top - bottom,
-                depth = zFar - zNear;
-
-            return [
-                2.0 / width,
-                0.0,
-                0.0,
-                0.0,
-
-                0.0,
-                2.0 / height,
-                0.0,
-                0.0,
-
-                0.0,
-                0.0,
-                -2.0 / depth,
-                0.0,
-
-                -(right + left) / width,
-                -(top + bottom) / height,
-                -(zFar + zNear) / depth,
-                1.0
-            ];
-        };
+        maxj;
 
     gl = GLSLUtilities.getGL(canvas);
     if (!gl) {
@@ -145,25 +64,6 @@
         father
     ];
 
-    // for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
-    //     objectsToDraw[i].buffer = GLSLUtilities.initVertexBuffer(gl,
-    //             objectsToDraw[i].vertices);
-
-    //     if (!objectsToDraw[i].colors) {
-    //         objectsToDraw[i].colors = [];
-    //         for (j = 0, maxj = objectsToDraw[i].vertices.length / 3;
-    //                 j < maxj; j += 1) {
-    //             objectsToDraw[i].colors = objectsToDraw[i].colors.concat(
-    //                 objectsToDraw[i].color.r,
-    //                 objectsToDraw[i].color.g,
-    //                 objectsToDraw[i].color.b
-    //             );
-    //         }
-    //     }
-    //     objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl,
-    //             objectsToDraw[i].colors);
-    // }
-
     shaderProgram = GLSLUtilities.initSimpleShaderProgram(
         gl,
         $("#vertex-shader").text(),
@@ -195,22 +95,6 @@
     modelViewMatrix = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
     projectionMatrix = gl.getUniformLocation(shaderProgram, "projectionMatrix");
 
-    // drawObject = function (object) {
-    //     gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
-    //     gl.vertexAttribPointer(vertexColor, 3, gl.FLOAT, false, 0, 0);
-
-    //     gl.uniformMatrix4fv(modelViewMatrix, gl.FALSE, new Float32Array(object.axis ?
-    //             getRotationMatrix(currentRotation, object.axis.x, object.axis.y, object.axis.z) :
-    //             [1, 0, 0, 0,
-    //              0, 1, 0, 0,
-    //              0, 0, 1, 0,
-    //              0, 0, 0, 1]
-    //         ));
-
-    //     gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
-    //     gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
-    //     gl.drawArrays(object.mode, 0, object.vertices.length / 3);
-    // };
 
     for (var i = 0; i < objectsToDraw.length; i++) {
         objectsToDraw[i].g_ready(gl);
